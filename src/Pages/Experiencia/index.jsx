3cb4769b7a9dd657c49d3c14ad617f31c90fc968
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-09-09 08:53:03 -0300

    :recycle: Modificando texto

--- src/Pages/Experiencia/index.jsx
+++ src/Pages/Experiencia/index.jsx
@@ -8,7 +8,8 @@ const Experiencias = () => {
       <TituloPagina>Experiências:</TituloPagina>
 
       <h3>Empresa:</h3>
-      <p>Horbiti - Marca criada por mim em 2020.</p>
+      <p>Horbiti - 2020 - Atualmente.</p>
+      <p>Horbiti é uma marca criada por mim focada na criação de sites e e-commerce.</p>
       <br></br>
       <h3>Cargo:</h3>
       <p>Freelance / Proprietário</p>
