5da0514e580eb52e5f74a585d76d3be85365add4
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-09-12 10:47:09 -0300

    :sparkles> Abrindo e fechando textos dinamicamente

--- src/Pages/Projetos/index.jsx
+++ src/Pages/Projetos/index.jsx
@@ -3,6 +3,7 @@ import FotoPerfil from "../../Components/Common/FotoPerfil";
 import { MdOutlineRemoveRedEye } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
 import { useBuscaProjetos } from "../../Hooks/useBuscaProjetos";
+import { useState } from "react";
 
 const SecaoProjetos = styled.section`
 
@@ -23,6 +24,28 @@ const SecaoProjetos = styled.section`
       font-family: "PoppinsMedium";
       font-size: .8rem  ;
     }
+  }
+
+  .textoHidde{
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    -webkit-line-clamp: 2;
+    text-overflow: ellipsis; 
+    line-height: 1.2rem;
+  }
+
+  .ver-mais{
+    cursor: pointer;
+    font-family: "PoppinsMedium";
+    text-align: center;
+    display: block;
+    width: 100%;
+    font-size: 12px;
+  }
+
+  .titulo{
+    font-family: "PoppinsMedium";
   }
 
   .imagem{
@@ -50,10 +73,22 @@ const SecaoProjetos = styled.section`
 const Projetos = () => {
 
   const {projetos, carregando, erro} = useBuscaProjetos();
+  const [textHidde, setTextHidde] = useState([]);
 
   if(carregando)return <p>Carregando...</p>;
 
   if(erro) return <p>{erro}</p>;
+
+  const abrirOuFecharTexto = (id) => {
+    setTextHidde((prevState) => {
+      if (prevState?.includes(id)) {
+        return prevState.filter((item) => item !== id);
+      } else {
+        return [...(prevState || []), id];
+      }
+    });
+    console.log(textHidde);
+  };
 
   return(
     <SecaoProjetos>
@@ -65,8 +100,9 @@ const Projetos = () => {
               <h4>rlxmts</h4>
             </div>
             <div>
-              <span> {item.titulo}</span>
-              <p>{item.texto}</p>
+              <span className="titulo"> {item.titulo}</span>
+              <p className={textHidde.includes(item._id) ? "" : "textoHidde"}>{item.texto}</p>
+              <span className="ver-mais" onClick={()=> abrirOuFecharTexto(item._id) } >{textHidde.includes(item._id) ? "ver menos" : "Ver mais"}</span>
             </div>
             <div  className="imagem">
               <img src={item.imagem} alt={item.titulo}/>
